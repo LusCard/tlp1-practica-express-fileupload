57b733e1a5618ad3c7accf8f60af888b55cde7b0
Estudiante <luquiscardenas@gmail.com>
2023-08-11 07:06:04 -0300

    no me acuerdo qué cambié pero por si acaso

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -13,12 +14,21 @@ const app = express();
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 
-
+//*MIDDLEWARES
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+app.use(fileUpload());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//!RUTAS
+app.post('/upload', (req, res) =>{
+    if(!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('Ea no me enviaste nada!!');
+    }
+})
+
 
 app.listen(process.env.PORT, ()=> console.log(`listening on http://localhost:${process.env.PORT}`));
